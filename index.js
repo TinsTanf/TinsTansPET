d9c361819f964fa4e1ed165aa31607a22a5c5e8e
Nguyen Tien Thanh <21021542@vnu.edu.vn>
2024-05-24 15:31:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// NAVBAR
+const navbar = document.querySelector("nav");
+window.addEventListener("scroll", () =>
+    navbar.classList.toggle("sticky", window.scrollY > 0)
+);
+
+const menu = document.querySelector(".menu");
+const toggleMenu = () => menu.classList.toggle("active");
+
+document.querySelector(".menu-btn").addEventListener("click", toggleMenu);
+document.querySelector(".close-btn").addEventListener("click", toggleMenu);
+
+document
+    .querySelector(".menu a")
+    .forEach((link) => link.addEventListener("click", toggleMenu));
